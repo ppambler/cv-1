3833f695ebc94db816ab0975511eb59423a80bec
Ambler <allinlbj@gmail.com>
2018-06-28 16:26:38 +0800

    「升级controller」

--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,14 +1,26 @@
-!function () {
+! function () {
     var view = document.querySelector('#topNavBar')
     view.style.border = '1px solid red'
-    var controller = function(view) {
-        window.addEventListener('scroll', function (x) {
-            if (window.scrollY > 0) {
-                view.classList.add('sticky')
-            } else {
-                view.classList.remove('sticky')
-            }
-        })
+    var controller = {
+        view: null,
+        // 这个初始化联想到巴贝奇差分机的运算（参见计算机专业导论）
+        init: function (view) {
+            this.view = view
+            this.bindEvents()
+            // this.bindEvents,call(this)
+        },
+        bindEvents: function () {
+            var view = this.view
+            window.addEventListener('scroll', function (x) {
+                if (window.scrollY > 0) {
+                    view.classList.add('sticky')
+                } else {
+                    view.classList.remove('sticky')
+                }
+            })
+        }
+
     }
-    controller(view)
+    controller.init(view)
+    // controller.init.call(controller,view)
 }.call()

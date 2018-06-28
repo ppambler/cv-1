24ab266e7db97d8975e3c824253a2b8655d49888
Ambler <allinlbj@gmail.com>
2018-06-28 16:09:02 +0800

    「简单的controller」

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,28 +1,32 @@
-!function () {
+! function () {
     var view = document.querySelector('#mySlides')
     view.style.border = '1px solid red'
     // console.dir(view)
-    // 这里的view.querySelector的用法我是没有想到的，我一直以为它只能
-    // 通过document.这样，没想到元素也可以这样
-    var mySwiper = new Swiper(view.querySelector('.swiper-container'), {
-        // Optional parameters
-        // direction: 'vertical',
-        loop: true,
+    var controller = function (view) {
+        // 这里的view.querySelector的用法我是没有想到的，我一直以为它只能
+        // 通过document.这样，没想到元素也可以这样
+        var mySwiper = new Swiper(view.querySelector('.swiper-container'), {
+            // Optional parameters
+            // direction: 'vertical',
+            loop: true,
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
 
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
 
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    })
+            // And if we need scrollbar
+            scrollbar: {
+                el: '.swiper-scrollbar',
+            },
+        })
+    }
+
+    controller(view)
 }.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,11 +1,14 @@
 !function () {
     var view = document.querySelector('#topNavBar')
     view.style.border = '1px solid red'
-    window.addEventListener('scroll', function (x) {
-        if (window.scrollY > 0) {
-            view.classList.add('sticky')
-        } else {
-            view.classList.remove('sticky')
-        }
-    })
+    var controller = function(view) {
+        window.addEventListener('scroll', function (x) {
+            if (window.scrollY > 0) {
+                view.classList.add('sticky')
+            } else {
+                view.classList.remove('sticky')
+            }
+        })
+    }
+    controller(view)
 }.call()

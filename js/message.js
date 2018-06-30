061388fdeb0f4afad6b28db1b0137c4f7ff2e020
Ambler <allinlbj@gmail.com>
2018-06-30 12:50:34 +0800

    「MVC之message模块VC化」

--- js/message.js
+++ js/message.js
@@ -1,67 +1,83 @@
-// 初始化demo
-var APP_ID = '5CggGB6NC7wymcDfvdKuUFDB-gzGzoHsz';
-var APP_KEY = 'ShdTwSrYm6RyqYguiUeGTkVa';
+! function () {
+  var view = document.querySelector('section.message')
+  var controller = {
+    view: null,
+    messageList: null,
+    form: null,
+    init: function (view) {
+      this.view = view
+      this.messageList = view.querySelector('#messageList')
+      this.form = view.querySelector('form')
+      this.initAV()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    initAV: function () {
+      // 初始化demo
+      var APP_ID = '5CggGB6NC7wymcDfvdKuUFDB-gzGzoHsz';
+      var APP_KEY = 'ShdTwSrYm6RyqYguiUeGTkVa';
 
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-// console.log('你能看到我吗？能看到的话，那么初始化就成功了，毕竟没有报错')
+      AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+      })
 
-// 获取某张表的数据
-// 这里实现JS的两大常用功能，一个是请求和DOM
-
-var query = new AV.Query('Message');
-query.find()
-  .then(
-    function (message) {
-      // console.log(message)
-      // 这里的map的作用，拿到所需要的内容然后封装成数组,有种过滤的味道
-      let array = message.map((item) => item.attributes)
-      // console.log(array)
-      array.forEach((item) => {
+    },
+    loadMessages: function () {
+      // 获取某张表的数据
+      // 这里实现JS的两大常用功能，一个是请求和DOM
+      var query = new AV.Query('Message');
+      query.find()
+        .then(
+          (message) => {
+            // console.log(message)
+            // 这里的map的作用，拿到所需要的内容然后封装成数组,有种过滤的味道
+            let array = message.map((item) => item.attributes)
+            // console.log(array)
+            array.forEach((item) => {
+              let li = document.createElement('li')
+              li.innerText = `${item.name}: ${item.content}`
+              // 如果这个形参为message的函数不用箭头函数表示的话，那么这个this就是window了
+              // console.log(this)
+              this.messageList.appendChild(li)
+            })
+          }
+        )
+    },
+    bindEvents: function () {
+      // 监听form表单的submit事件
+      this.form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        // console.log(this)
+        // 使用箭头函数，不然这个this的值就是这个form元素了，而不是这个controller
+        this.saveMessage()
+      })
+    },
+    saveMessage: function () {
+      // 这里用了一个中间变量，这样就不需要添加很多个this.form
+      // 之前不理解为啥芳芳说变量只用一次的话就不需要弄个变量出来了，如果需要多次出现那就搞个变量出来吧
+      // 如这个myForm
+      let myForm = this.form
+      let content = myForm.querySelector('input[name=content]').value
+      let name = myForm.querySelector('input[name=name]').value
+      // console.log(content)
+      var Message = AV.Object.extend('Message')
+      var message = new Message()
+      message.save({
+        'name': name,
+        'content': content
+      }).then(function (object) {
+        // 用户提交后然后就创建元素,不会和查询冲突
         let li = document.createElement('li')
-        li.innerText = `${item.name}: ${item.content}`
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
         messageList.appendChild(li)
+        // 清空内容
+        myForm.querySelector('input[name=content]').value = ''
+        // console.log(object)
       })
     }
-  )
-let myForm = document.querySelector('#postMessageForm')
-// 监听form表单的submit事件
-myForm.addEventListener('submit', function (e) {
-  e.preventDefault()
-  let content = myForm.querySelector('input[name=content]').value
-  let name = myForm.querySelector('input[name=name]').value
+  }
 
-  console.log(content)
-  var Message = AV.Object.extend('Message')
-  var message = new Message()
-  message.save({
-    'name': name,
-    'content': content
-  }).then(function (object) {
-    // 用户提交后然后就创建元素,不会和查询冲突
-    let li = document.createElement('li')
-    li.innerText = `${object.attributes.name}: ${object.attributes.content}`
-    let messageList = document.querySelector('#messageList')
-    messageList.appendChild(li)
-    // 清空内容
-    myForm.querySelector('input[name=content]').value = ''
-    console.log(object)
-  })
-})
-/*
-// 测试demo
-// 创建TestObject表
-var TestObject = AV.Object.extend('TestObject');
-// 在表中创建一行数据
-var testObject = new TestObject();
-// 保存数据内容是words: 'Hello World!'到testObject里去
-// 如果保存成功，则运行这个alert('')
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-*/
+  controller.init(view)
+}.call()
